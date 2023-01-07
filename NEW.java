9703016ae986c6dfec302f37ce0e4f0a94888edf
gowdaSach <sachingowda270496@gmail.com>
2023-01-07 15:19:16 +0530

    First Commit from IntelliJ

---
+++ NEW.java
@@ -0,0 +1,22 @@
+import javax.print.attribute.standard.MediaName;
+
+public class NEW
+{
+    public static void main(String args[])
+    {
+
+        int j=10+1;
+        System.out.println(j);
+        System.out.println(j);
+        System.out.println(j);
+        System.out.println(j);
+        menu();
+
+
+    }
+    public static void menu(){
+        System.out.println("Hi");
+
+    }
+
+}
