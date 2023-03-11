0e43be078b955a016fb1b759733e7fa41ba9c357
gowdaSach <sachingowda270496@gmail.com>
2023-03-11 14:07:28 +0530

    Updated files

--- src/main/java/testrunner/TestRunner.java
+++ src/main/java/testrunner/TestRunner.java
@@ -9,8 +9,9 @@ import org.junit.runner.RunWith;
     @RunWith(Cucumber.class)
     @CucumberOptions(
             features = ".//src/test/java/features",
-            glue="",
+            glue="stepDefinitions",
             dryRun = true,
+            tags = "@NEW-2023",
             plugin = {"pretty","html:test-output",
                                 "junit:target/cucumber-reports/Cucumber.xml"}
     )
--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,5 +1,6 @@
 Feature: Login to DemoGuru99 Page
 
+  @NEW-2023
   Scenario Outline: User login to DemoGuru99 page with valid credentials
     Given User launches chrome browser
     And User search for the URL "https://demo.guru99.com/test/login.html"
